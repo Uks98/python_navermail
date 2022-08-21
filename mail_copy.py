5f5b4d8c10e3c8ccd71f2db1a2d0b4e3e2bba9fd
uks98 <rozyfactory@gmail.com>
2022-08-21 21:37:40 +0900

    '전송할메일주소있을시자동전송'

--- mail_copy.py
+++ mail_copy.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 import pyautogui
 import pyperclip
 
-my_id = "bareman9"
-my_pw = "265133kk45!"
+my_id = ""
+my_pw = ""
 
-mail_list = ["bareman9@naver.com","rozyfactory@gmail.com"]
+mail_list = ["bareman9@naver.com","      rozyfactory@gmail.com","      bareman12@gmail.com"]
 url = "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com"
 
 browser = webdriver.Chrome("c://chromedriver")
@@ -50,32 +50,37 @@ send_mail = browser.find_element(By.CSS_SELECTOR, "#nav_snb > div.btn_workset > 
 send_mail.click()
 time.sleep(1)
 
+
+
+for mail in range(len(mail_list)):
 #받는사람
-browser.find_element(By.CSS_SELECTOR,"#toInput")
+    browser.find_element(By.CSS_SELECTOR,"#toInput")
+    pyautogui.write(mail_list[mail],interval= 0.05) #0.25초 간격을 준다
 # pyautogui.press("tab") #tab키를 눌러서 전환
 # pyautogui.press("tab")
-time.sleep(1)
+    time.sleep(1)
 
 #제목 
-browser.find_element(By.CSS_SELECTOR,"#subject").click()
-pyperclip.copy("이 메일은 파이썬으로 보내는 메일입니다.") #파이썬에서 한글로 작성할때 사용
-pyautogui.hotkey("ctrl","v")
-time.sleep(1)
+    browser.find_element(By.CSS_SELECTOR,"#subject").click()
+    pyperclip.copy("이 메일은 파이썬으로 보내는 메일입니다.") #파이썬에서 한글로 작성할때 사용
+    pyautogui.hotkey("ctrl","v")
+    time.sleep(1)
 
 #iframe 안으로 들어가기
 #본문 태그의 부모를 찾는다
-browser.switch_to.frame("se2_iframe") 
+    browser.switch_to.frame("se2_iframe") 
 #본문 내용 작성
 #클래스 선택자는 ctrl + f누른 후 . 을 통해 식별자 찾는다.
-browser.find_element(By.CSS_SELECTOR,".se2_inputarea").send_keys("이 메일은 테스트용으로 작성되었습니다.")
-time.sleep(1)
+    browser.find_element(By.CSS_SELECTOR,".se2_inputarea").send_keys("이 메일은 테스트용으로 작성되었습니다.")
+    time.sleep(1)
 #iframe 밖으로 나가기
-browser.switch_to.default_content()
+    browser.switch_to.default_content()
 #메일 전송하기 
-browser.find_element(By.CSS_SELECTOR,"#sendBtn").click()
-time.sleep(1)
+    browser.find_element(By.CSS_SELECTOR,"#sendBtn").click()
+    time.sleep(3)
 
 #재전송
-browser.find_element(By.XPATH,'//span[text() = "메일쓰기"]').click()
+    browser.find_element(By.XPATH,'//span[text() = "메일쓰기"]').click()
+    time.sleep(3)
 
 
